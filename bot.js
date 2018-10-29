101520f5c29da4bea3b5bcb7229332f0f8c09e6c
Joan Olucha <joanmaria.alemania@gmail.com>
2018-10-29 16:50:35 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,10 +6,10 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
 
+    
     console.log(`${client.user.username} Has Been Started`);
 
 });
-
 
 
 
@@ -23,13 +23,31 @@ client.on('message', async message => {
    
     
     let args = message.content.slice(prefix.length).trim().split(' ');
+    
+    
     let cmd = args.shift().toLowerCase();
     
+    
     if (message.channel.type == "dm") {
-         console.log(`${message.author.tag} Said ${message.content}`);
+       
+        console.log(`${message.author.tag} Said ${message.content}`);
+        
     }
    
 if (message.channel.type == "text") {
+    var permdenyEmbed = new Discord.RichEmbed()
+.setColor('#ff4d4d')
+ .setDescription(`<@${message.author.id}>, Sorry, you don't meet the permission level for this command!`)
+.setTitle('Permission Denied!')
+    
+    var noMention = new Discord.RichEmbed()
+.setColor('#ff4d4d')
+ .setDescription(`<@${message.author.id}>, The User that you mentioned was not found!`)
+.setTitle('User Not Found!')
+    
+   
+        
+         
    function function1() {
     message.delete(0)
 }
@@ -39,38 +57,79 @@ if (message.channel.type == "text") {
        setTimeout(function1, 3000);
       
     }
-   if (cmd === `${prefix}ping`) {
-      message.delete().catch();
-       if (!message.member.hasPermission(['MANAGE_GUILD'])) return message.reply("You don't have enough permission to do that!");
-       metion = message.mentions.users.first();
-       if (metion == null) return message.reply("You didin't mention anyone!");
-      //authername = message.author.Username
-       message.reply("Message Sent!");
-       metion.send(message.author.username +' Has Pinged You!');
+  
+    
+    if (message.content.startsWith(prefix))  {
+   // if (cmd == 'ping') {
+      // message.delete().catch();
+       // if (!message.member.hasPermission(['MANAGE_GUILD'])) return message.channel.sendEmbed(permdenyEmbed);
+       // metion = message.mentions.users.first();
+       // if (metion == null) return message.reply("You didin't mention anyone!");
+      // //authername = message.author.Username
+       // message.reply("Message Sent!");
+       // metion.send(message.author.username +' Has Pinged You!');
  
-}
-     if (cmd === `${prefix}warn`) {
+// }
+     if (cmd == 'warn') {
+         
          message.delete().catch();
-        
-          if (!message.member.hasPermission(['KICK_MEMBERS'])) return message.reply("You don't have enough permission to do that!");
+       let reportchannel = message.guild.channels.find(c => c.name === "logging");
+          if (!message.member.hasPermission(['KICK_MEMBERS'])) return message.channel.sendEmbed(permdenyEmbed);
          let mUser = message.guild.member(message.mentions.users.first() || message.guild.member.get(args[0]));
-         if(!mUser) return message.reply("Couldn't find that user!")
+         if(!mUser) return message.reply("Couldn't find that user!");
          let dmMsg = args.join(" ").slice(22);
          mUser.send(`You have been warned for ${dmMsg}`);
-         let warnlogembbed = new Discord.RichEmbed()
+        
+           var SuccessWarn = new Discord.RichEmbed()
+.setColor('#99ff66')
+ .setDescription(`${mUser.username} Has been warned!`)
+.setTitle('User Has Been Warned!')
+         
+         var warnlogembbed = new Discord.RichEmbed()
          .setDescription('Warning')
          .addField(`this is a test`, `this is a test`)
-          message.channel.send(warnlogembbed);
+         .addField("fisrst value", "secound value")
+         .setColor('#edc607')
+         message.channel.sendEmbed(SuccessWarn);
+          reportchannel.sendEmbed(warnlogembbed);
       
        
  
 }
-    }
+        
+         if (cmd == 'help') {
+         message.delete().catch();
+              
+       var everyonecommand = new Discord.RichEmbed()
+       .setTitle('Command List - Everyone')
+         .setDescription('Commands That Everyone Can User')
+         .addField(`${prefix}help`, `Shows list of commands`)
+       var admincommand = new Discord.RichEmbed()
+       .setTitle('Command List - Administators')
+         .setDescription('Commands That Only Administrators Can Use')
+         .addField(`${prefix}warn`, `Warns a user!`)
+        .addField(`${prefix}dm`, `Sends Private Message to users - Used For Support Tickets`)
+        message.channel.sendEmbed(everyonecommand)
+             message.channel.sendEmbed(admincommand)
+}
+        
+         if (cmd == 'dm') {
+         message.delete().catch();
+      
+          if (!message.member.hasPermission(['KICK_MEMBERS'])) message.channel.sendEmbed(permdenyEmbed);;
+           let mUser = message.guild.member(message.mentions.users.first() || message.guild.member.get(args[0]));
+         if(!mUser) return message.reply("Couldn't find that user!")
+         let dmMsg = args.join(" ").slice(22);
+         mUser.send(`${dmMsg}`);
+      
+         
+         
+      
+       
+ 
+}
+}   
+}
 });
 
-
-
-// THIS  MUST  BE  THIS  WAY
-
 client.login(process.env.BOT_TOKEN);
-
