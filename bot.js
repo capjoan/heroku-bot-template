425738445e2c10e19b08e15a966b2a5996e6f6bc
Joan Olucha <joanmaria.alemania@gmail.com>
2018-11-06 10:52:14 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -131,7 +131,13 @@ if (message.channel.type == "text") {
          if(!mUser) return message.reply("Couldn't find that user!")
          let dmMsg = args.join(" ").slice(22);
          mUser.send(`${dmMsg}`);
-      
+      if(command === "ping") {
+    // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
+    // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
+    const m = await message.channel.send("Ping?");
+    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+  }
+  
          
          
       
