de16a668d50c5dd26fe98ea9c803e1a717054236
Joan Olucha <joanmaria.alemania@gmail.com>
2018-11-06 13:30:17 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,18 +3,14 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
-    function changing_status() {
-        let status = ['!help | Prefix: !', 'under development!', 'utbot.wadj.ga', 'NEW: Set log channel!', 'Alpha thing', `${client.users.size} users!`, `on ${client.guilds.size} servers`]
-        let random = status[Math.floor(Math.random() * status.length)]
-        client.user.setActivity(random)
-      }
-
-client.on('ready', () => {
-
-  
-    console.log(`${client.user.username} Has Been Started`);
-
+client.on("ready", () => {
+  // This event will run if the bot starts, and logs in, successfully.
+  console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
+  // Example of changing the bot's playing game to something useful. `client.user` is what the
+  // docs refer to as the "ClientUser".
+  client.user.setActivity(`Serving ${client.guilds.size} servers`);
 });
+ 
 
 
 
