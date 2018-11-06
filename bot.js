100b92b0d03fd9ec2586bf9fc27608aaf3dc989b
Joan Olucha <joanmaria.alemania@gmail.com>
2018-11-06 11:04:50 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -153,23 +153,7 @@ if (message.channel.type == "text") {
 }
 
 
-      if(cmd == 'ping') {
-    // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
-    // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
-    const m = await message.channel.send("Ping?");
-    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
-  }
-  
-         
-     if(cmd == 'say') {
-    // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
-    // To get the "message" itself we join the `args` back into a string with spaces: 
-    const sayMessage = args.join(" ");
-    // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
-    message.delete().catch(O_o=>{}); 
-    // And we get the bot to say the thing: 
-    message.channel.send(sayMessage);
-  }    
+      
       
        
  
